e6d722ea8ced55e8a02fd1103dd0f1c7eb599a73
fozlerabbi321 <fozlerabbiinfo@gmail.com>
2021-09-09 11:19:08 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+void main(){
+  var name = 'Fozle';
+  // I Love Bangladesh.
+  print('Noman Islam  ${1 + 50}');
+  print('Noman Islam  111 $name');
+
+
+  print(name);
+
+  int f;
+  f = 1;
+
+  var num = 1;
+  var Num = 200;
+  //$ and _ others sp
+
+  // A = 68;
+  // a = 98;
+  void block(){//Start block
+    // Statement
+    // Equ
+  }//End Block
+
+  //Single-line Comments
+  /*Multi-line Comments*/
+  ///Documentation Comments
+
+}
