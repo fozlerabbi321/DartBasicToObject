cae3a9074d8191c0c22b5928f71c8c7ddc3a1685
fozlerabbi321 <fozlerabbiinfo@gmail.com>
2021-09-12 11:46:36 -0700

    3rd class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 void main(){
-  var name = 'Fozle';
+  /*var name = 'Fozle';
   // I Love Bangladesh.
   print('Noman Islam  ${1 + 50}');
   print('Noman Islam  111 $name');
@@ -13,7 +13,7 @@ void main(){
   var num = 1;
   var Num = 200;
   //$ and _ others sp
-
+*/
   // A = 68;
   // a = 98;
   void block(){//Start block
@@ -25,4 +25,43 @@ void main(){
   /*Multi-line Comments*/
   ///Documentation Comments
 
+  // Dart Variable
+  // a = 20\
+  // x = 30
+  // a = 20
+  //variableName = value
+  final a = 20; // variableName = value;
+  var ar = 20;
+  int number = 10;
+  var yourNameRaFa = 'Fozle Rabbi';
+
+  print(yourNameRaFa);
+  int num = 20;
+  /// Data type in Dart
+  // Number, -- int - double
+  // String,
+  // List
+  // Boolean
+  // Map
+  // Final and Const
+
+  //  200, 10.0, 2747,
+  // int = 54, 457, 2475
+  // double = 20.0, 2555.0005l, 245.5574
+
+  int n = 54;
+  double d = 203567547;
+  // 64 bit
+  String name = "ta by the  modify. fkadjklfajd" +" Ja " + yourNameRaFa ;
+  String text = 'The data types are the most important';
+
+  print(name);
+
+  bool b = false;// true and false
+  if(b){
+    print("Fozle Rabbi");
+  }else{
+    print("fff");
+  }
+  
 }
